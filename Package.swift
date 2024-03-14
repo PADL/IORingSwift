4f0abf92841c626efb2d5793490b129babfe58e5
Luke Howard <lukeh@padl.com>
2024-03-15 08:16:57 +1100

    update to swift-log 1.5.4

--- Package.swift
+++ Package.swift
@@ -64,7 +64,7 @@ let package = Package(
     .package(url: "https://github.com/apple/swift-async-algorithms", from: "0.1.0"),
     .package(url: "https://github.com/lhoward/AsyncExtensions", branch: "linux"),
     .package(url: "https://github.com/dfed/swift-async-queue", from: "0.4.0"),
-    .package(url: "https://github.com/apple/swift-log", branch: "main"),
+    .package(url: "https://github.com/apple/swift-log", from: "1.5.4"),
   ],
   targets: [
     .systemLibrary(
